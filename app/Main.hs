14ecbd1d885f3e45bc203cdd2b42c9b36ee62631
timput <tim@timput.com>
2020-07-23 19:32:25 -0600

    small cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -12,10 +12,9 @@ main = do
     let (w,h) = (512,512)
         f n = round $ (255/ fromIntegral w) * (fromIntegral n)
         f' n = round $ (255/ fromIntegral w) * sqrt(fromIntegral n)
-        vs = [(511,0),(400,200),(400,512-1),(512-1,512-1)]
+        vs = [(511,0),(400,200),(400,511),(511,511)]
         vs' = fmap (\a -> (256 + (round $ 100 * cos a), 256 + (round $ 100 * sin a))) [0,1..2*pi]
     let j = concat
-            -- . fmap (\(x,y,d) -> let d' = f' d in [d', d', d', 0])
             . fmap (\(x,y,d) -> let d' = f' d in [d', d', d', 0])
             . V.toList $ jfa (vs ++ vs') w h
     let rgba = BS.pack j
